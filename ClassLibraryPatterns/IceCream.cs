9955dd440ba4d3c17042890a4c1f895255e042d1
UsikovKonstantin <99312736+UsikovKonstantin@users.noreply.github.com>
2023-03-31 08:33:38 +0300

    Паттерн "Декоратор" стандартная реализация

---
+++ ClassLibraryPatterns/IceCream.cs
@@ -0,0 +1,181 @@
+﻿namespace ClassLibraryPatterns
+{
+    /* Паттерн "Декоратор" стандартная реализация */
+
+    #region Абстрактный класс того, что будем декорировать
+    /// <summary>
+    /// Абстракный класс "Мороженое".
+    /// </summary>
+    public abstract class IceCream
+    {
+        protected string description = "";
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        public string Description
+        {
+            get { return description; }
+        }
+
+        /// <summary>
+        /// Реализуется каждым потомком.
+        /// </summary>
+        /// <returns> стоимость мороженого </returns>
+        public abstract double Cost();
+
+        public override string ToString()
+        {
+            return description + "\n" + "cost: " + Cost();
+        }
+    }
+    #endregion
+
+    #region Абстрактный класс декоратор
+    /// <summary>
+    /// Родитель декораторов, задающих добавки к мороженому.
+    /// </summary>
+    public abstract class CondimentDecorator : IceCream
+    {
+        /// <summary>
+        /// Реализуется потомками.
+        /// </summary>
+        /// <returns> возвращает описание напитка со всеми добавками </returns>
+        public abstract string GetDescription();
+
+        /// <summary>
+        /// Вложенный декорируемый объект.
+        /// </summary>
+        protected IceCream complex;
+    }
+    #endregion
+
+    #region Конкретные классы того, что будем декорировать
+    /// <summary>
+    /// Пломбир.
+    /// </summary>
+    public class Plombier : IceCream
+    {
+        public Plombier()
+        {
+            description += "Мороженое пломбир ";
+        }
+
+        public override double Cost()
+        {
+            return 20.50;
+        }
+    }
+
+    /// <summary>
+    /// Сливочное мороженое.
+    /// </summary>
+    public class Sundae : IceCream
+    {
+        public Sundae()
+        {
+            description += "Мороженое сливочное ";
+        }
+
+        public override double Cost()
+        {
+            return 15.50;
+        }
+    }
+
+    /// <summary>
+    /// Молочное мороженое.
+    /// </summary>
+    public class MilkIceCream : IceCream
+    {
+        public MilkIceCream()
+        {
+            description += "Мороженое молочное ";
+        }
+
+        public override double Cost()
+        {
+            return 10.50;
+        }
+    }
+    #endregion
+
+    #region Конкретные классы декораторы
+    /// <summary>
+    /// Шоколадная стружка.
+    /// </summary>
+    public class ChocolateChips : CondimentDecorator
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="ic"> встроенный объект, декорируется шоколадной стружкой </param>
+        public ChocolateChips(IceCream ic)
+        {
+            complex = ic;
+            description = ic.Description + " Шоколадная стружка ";
+        }
+
+        public override string GetDescription()
+        {
+            return complex.Description + " Шоколадная стружка ";
+        }
+
+        public override double Cost()
+        {
+            return complex.Cost() + 5.50;
+        }
+    }
+
+    /// <summary>
+    /// Орешки.
+    /// </summary>
+    public class Nuts : CondimentDecorator
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="ic"> встроенный объект, декорируется орешками </param>
+        public Nuts(IceCream ic)
+        {
+            complex = ic;
+            description = ic.Description + " Орешки ";
+        }
+
+        public override string GetDescription()
+        {
+            return complex.Description + " Орешки ";
+        }
+
+        public override double Cost()
+        {
+            return complex.Cost() + 3.50;
+        }
+    }
+
+    /// <summary>
+    /// Малиновый сироп.
+    /// </summary>
+    public class RaspberrySyrup : CondimentDecorator
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="ic"> встроенный объект, декорируется малиновым сиропом </param>
+        public RaspberrySyrup(IceCream ic)
+        {
+            complex = ic;
+            description = ic.Description + " Малиновый сироп ";
+        }
+
+        public override string GetDescription()
+        {
+            return complex.Description + " Малиновый сироп ";
+        }
+
+        public override double Cost()
+        {
+            return complex.Cost() + 6.50;
+        }
+    }
+    #endregion
+}
--- ConsolePatterns/Program.cs
+++ ConsolePatterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ClassLibraryPatterns;
 
+#region Паттерн "Стратегия"
 BallPointPen bpp = new BallPointPen();
 Console.WriteLine(bpp.Display());
 Console.WriteLine(bpp.PerformWrite());
@@ -19,3 +20,24 @@ CapillaryPen cp = new CapillaryPen();
 Console.WriteLine(cp.Display());
 Console.WriteLine(cp.PerformWrite());
 Console.WriteLine();
+#endregion
+
+#region Паттерн "Декоратор" стандартная реализация
+IceCream plompier = new Plombier();
+plompier = new ChocolateChips(plompier);
+Console.WriteLine(plompier.Description);
+Console.WriteLine();
+
+IceCream sundae = new Sundae();
+sundae = new RaspberrySyrup(sundae);
+sundae = new Nuts(sundae);
+Console.WriteLine(sundae.Description);
+Console.WriteLine();
+
+IceCream mic = new MilkIceCream();
+mic = new ChocolateChips(mic);
+mic = new RaspberrySyrup(mic);
+mic = new Nuts(mic);
+Console.WriteLine(mic.Description);
+Console.WriteLine();
+#endregion
